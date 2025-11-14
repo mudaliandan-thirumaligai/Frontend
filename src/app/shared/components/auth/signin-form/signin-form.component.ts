1ce45b144ce3bdc5dd9bc218940103ac58132107
Srinidhi Krishnan <kadalazhisrinidhi2103@gmail.com>
2025-11-14 17:51:30 +0530

    Initial Angular Codebase

---
+++ src/app/shared/components/auth/signin-form/signin-form.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { LabelComponent } from '../../form/label/label.component';
+//import { CheckboxComponent } from '../../form/input/checkbox.component';
+import { ButtonComponent } from '../../ui/button/button.component';
+import { InputFieldComponent } from '../../form/input/input-field.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-signin-form',
+  imports: [
+    CommonModule,
+    LabelComponent,
+   // CheckboxComponent,
+    ButtonComponent,
+    InputFieldComponent,
+    RouterModule,
+    FormsModule,
+  ],
+  templateUrl: './signin-form.component.html',
+  styles: ``
+})
+export class SigninFormComponent {
+
+  showPassword = false;
+  // isChecked = false;
+
+  username = '';
+  password = '';
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+  // TODO need to add api calling here 
+
+  onSignIn() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    // console.log('Remember Me:', this.isChecked);
+  }
+}
---
+++ src/app/shared/components/auth/signup-form/signup-form.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { LabelComponent } from '../../form/label/label.component';
+import { CheckboxComponent } from '../../form/input/checkbox.component';
+import { InputFieldComponent } from '../../form/input/input-field.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// TODO to make this an admin page only page in future
+@Component({
+  selector: 'app-signup-form',
+  imports: [
+    CommonModule,
+    LabelComponent,
+    CheckboxComponent,
+    InputFieldComponent,
+    RouterModule,
+    FormsModule,
+  ],
+  templateUrl: './signup-form.component.html',
+  styles: ``
+})
+export class SignupFormComponent {
+
+  showPassword = false;
+  isChecked = false;
+
+  username = '';
+  // lname = '';
+  email = '';
+  password = '';
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSignIn() {
+    console.log('First Name:', this.username);
+    // console.log('Last Name:', this.lname);
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Remember Me:', this.isChecked);
+  }
+}
