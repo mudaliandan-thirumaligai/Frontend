892487261160b0b93e9a1592955bc3c24f1cfeec
Srinidhi Krishnan <kadalazhisrinidhi2103@gmail.com>
2026-01-17 09:34:11 +0530

    added prod creds

---
+++ src/app/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://thirumaaligai-website.onrender.com'
+};
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,1 +1,4 @@
-export const environment = { production: false, apiUrl: 'https://thirumaaligai-website.onrender.com' };
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080'
+};
