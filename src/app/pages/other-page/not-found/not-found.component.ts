b48de89d76b04791cfb9d8568799747aceb9fad4
Srinidhi Krishnan <kadalazhisrinidhi2103@gmail.com>
2026-02-02 18:59:06 +0530

    Cleanup

--- src/app/pages/other-page/not-found/not-found.component.ts
+++ src/app/pages/other-page/not-found/not-found.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { GridShapeComponent } from '../../../shared/components/common/grid-shape/grid-shape.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   imports: [
-    GridShapeComponent,
     RouterModule,
   ],
   templateUrl: './not-found.component.html',
