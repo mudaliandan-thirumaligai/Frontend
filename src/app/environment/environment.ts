d8c17acf6ded8b8fb45b0d8154d95301be5c04f3
Srinidhi Krishnan <kadalazhisrinidhi2103@gmail.com>
2026-03-08 13:47:26 +0530

    Changed env to prod domain

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
-  // apiUrl: 'https://api.mudaliandan.org'
+  // apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://api.mudaliandan.org'
 };
