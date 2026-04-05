fe752bd9be07326fba68e5068ef34b51fa3ff393
Srinidhi Krishnan <kadalazhisrinidhi2103@gmail.com>
2026-04-05 10:07:33 +0530

    Added vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { inject } from '@vercel/analytics';
 import {
   HTTP_INTERCEPTORS,
   provideHttpClient,
@@ -10,6 +11,7 @@ import {
 import { RequestLogger } from './app/interceptor/network.interceptor';
 import { AuthInterceptor } from './app/interceptor/auth.interceptor';
 
+inject();
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
